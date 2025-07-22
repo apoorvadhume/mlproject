2a9515b46c2ada43fdab4233e8c5f84c2c966719
Apoorva Dhume <apoorvadhume@gmail.com>
2025-07-22 16:57:13 -0700

    EDA performed

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -24,4 +25,4 @@ if __name__=="__main__":
         a=1/0
     except Exception as e:
         logging.info("Divide by Zero Error")
-        raise CustomException(str(e), sys.exc_info())
+        raise CustomException(e, sys)
